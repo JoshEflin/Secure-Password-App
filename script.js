228dd435a94e1e28b14a71ad368af4389114c1ee
ragingpenguin <eflinjh@gmail.com>
2022-12-11 20:48:00 -0500

    added comments, changed some var's to const's

--- script.js
+++ script.js
@@ -17,47 +17,44 @@ for (i = 0; i < 26; i++) {
 }
 
 const mainArray = [upperCase, lowerCase, numArray, charArray];
-const prompts = ["uppercase letters", "lowercase letters", "special characters","numbers"]
+const prompts = ["uppercase letters", "lowercase letters", "special characters", "numbers"]
 // charTypes declared  globally to be used in two functions
-var charTypes = document.querySelector(".card");
+const charTypes = document.querySelector(".card");
 
 // if an invalid input is entered, user can try again. If user fails again, the writePassword function starts over at the beginning
 function errorMessage() {
   alert("The length you have chosen does not fall in range 8-128. Please input a number between 8 and 128");
   writePassword();
 }
+// The removePrevious()function removes <p> tags that have been appended to the div.card incase the user would like to generate more passwords 
 function removePrevious() {
-  var child = document.querySelectorAll(".test");
-  console.log(child)
-    for (i = 0; i <child.length; i ++){
-      charTypes.removeChild(child[i]);
-    }
+  let child = document.querySelectorAll(".test");
+  for (i = 0; i < child.length; i++) {
+    charTypes.removeChild(child[i]);
+  }
 }
 // Write password to the #password input
 function writePassword() {
   // declare variables locally so that each time the function is run, the main array and previous password are removed (in case the user has run the function several times)
   // step 1: create an empty array, and an empty string. These will be used later
 
-
   var userArray = [];
   var password = "";
   alert("Hello! you are about to generate a password. Please press 'OK' to choose parameters")
-  
+
   var userLength = prompt("Your password must be between 8 and 128 characters. How many characters do you want it to be?");
   //  In case user inputs a string, mulitply string by a number. This will change its type to NaN which we can test for using the .is() method and send the user to our error message.
   var numTest = userLength * 2;
-  if (Object.is(numTest, NaN)) {
-    console.log(numTest);
+  if (Object.is(numTest, NaN) || userLength < 8 || userLength > 128) {
     errorMessage();
     return;
   }
-  if (userLength < 8 || userLength > 128) {
-    errorMessage();
-    // return statement, prevents  the below code from executing  in the event of an invalid input, otherwise the below code will run AFTER the writePassword call on ln 22
-    return;
-  }
+  // return statement, prevents  the below code from executing  in the event of an invalid input, otherwise the below code will continue to
+  // run AFTER the writePassword() call  within the  errormessage() function
+
   removePrevious();
   // ask the user  which arrays should be inlcuded in the password generator algorith. .push() them to the userArray, and append them to the HTML in case the user forgot selection
+  // if the user is running the function multiple times removePrevious will restore the HTML file to its original state.
   for (i = 0; i < mainArray.length; i++) {
 
     confirmResponse = confirm("would you like to include " + prompts[i] + ": 'OK' for 'YES' ; 'Cancel' for 'NO'")
@@ -68,12 +65,11 @@ function writePassword() {
       newElement.textContent = "You selected to use " + prompts[i];
       alert(" You have selected to include " + prompts[i]);
       charTypes.appendChild(newElement)
-    } 
-      else {
-        alert("You have selected NOT to include " + prompts[i])
-      }
+    }
+    else {
+      alert("You have selected NOT to include " + prompts[i])
+    }
   }
-
   //  step 2:  write a for loop to use a randomly generated number to select a nested array, and select a  
   //  value from it, the loop terminates when the length of the password string reaches userLength
   for (i = 0; i < userLength; i++) {
